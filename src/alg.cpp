715f2db2709fccf02626794a5b4270551884a05e
Anton434 <78924192+Anton434@users.noreply.github.com>
2021-06-01 13:57:09 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,13 +1,8 @@
 // Copyright 2021 NNTU-CS
 
 int countPairs1(int *arr, int len, int value) {
-  return 0;
-}
-
-int countPairs2(int *arr, int len, int value) {
-  return 0;
-}
-
-int countPairs3(int *arr, int len, int value) {
-  return 0;
-}
+int sch=0;
+int sch = 0;
+for(int i = 0; i < len-1; i++) {
+for(int j = i + 1; j < len; j++) {
+if (arr[i] + arr[j] == value) sch++;
